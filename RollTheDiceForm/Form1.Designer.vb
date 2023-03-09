01b4f0e7c17836924a59f6a23c33910466299a0e
bbre1666 <badenbrenner@isu.edu>
2023-03-09 08:21:37 -0700

    Add project files.

---
+++ RollTheDiceForm/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RollTheDice
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(147, 333)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(143, 77)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(328, 333)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(143, 77)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(511, 333)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(143, 77)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(147, 22)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(507, 264)
+        Me.ListBox1.TabIndex = 4
+        '
+        'RollTheDice
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Name = "RollTheDice"
+        Me.Text = "Roll The Dice"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ RollTheDiceForm/Form1.vb
@@ -0,0 +1,110 @@
+﻿Option Explicit On
+Option Strict On
+'Baden Brenner
+'RCET0265
+'spring 2023
+'RolltheDiceForm
+
+Public Class RollTheDice
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+
+    End Sub
+
+
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim rollValue As Integer
+        Dim rollCount(11, 1) As Integer
+        Dim numberofRolls As Integer
+        Dim printcount As Integer
+        Dim leave As Boolean = False
+        Dim temp As String
+        'setting the column labels 
+        rollCount(0, 0) = 1
+        rollCount(1, 0) = 2
+        rollCount(2, 0) = 3
+        rollCount(3, 0) = 4
+        rollCount(4, 0) = 5
+        rollCount(5, 0) = 6
+        rollCount(6, 0) = 7
+        rollCount(7, 0) = 8
+        rollCount(8, 0) = 9
+        rollCount(9, 0) = 10
+        rollCount(10, 0) = 11
+        rollCount(11, 0) = 12
+
+        'Reseting The Array
+        rollCount(0, 1) = 0
+        rollCount(1, 1) = 0
+        rollCount(2, 1) = 0
+        rollCount(3, 1) = 0
+        rollCount(4, 1) = 0
+        rollCount(5, 1) = 0
+        rollCount(6, 1) = 0
+        rollCount(7, 1) = 0
+        rollCount(8, 1) = 0
+        rollCount(9, 1) = 0
+        rollCount(10, 1) = 0
+        rollCount(11, 1) = 0
+
+        Do Until numberofRolls = 1000
+            Dim Dice1 As Integer
+            Randomize()
+            Dice1 = CInt(Int((6 * Rnd()) + 1))
+
+            Dim Dice2 As Integer
+            Randomize()
+            Dice2 = CInt(Int((6 * Rnd()) + 1))
+
+            rollValue = (Dice1 + Dice2)
+
+            If rollValue = 1 Then
+                rollCount(0, 1) = rollCount(0, 1) + 1
+            ElseIf rollValue = 2 Then
+                rollCount(1, 1) = rollCount(1, 1) + 1
+            ElseIf rollValue = 3 Then
+                rollCount(2, 1) = rollCount(2, 1) + 1
+            ElseIf rollValue = 4 Then
+                rollCount(3, 1) = rollCount(3, 1) + 1
+            ElseIf rollValue = 5 Then
+                rollCount(4, 1) = rollCount(4, 1) + 1
+            ElseIf rollValue = 6 Then
+                rollCount(5, 1) = rollCount(5, 1) + 1
+            ElseIf rollValue = 7 Then
+                rollCount(6, 1) = rollCount(6, 1) + 1
+            ElseIf rollValue = 8 Then
+                rollCount(7, 1) = rollCount(7, 1) + 1
+            ElseIf rollValue = 9 Then
+                rollCount(8, 1) = rollCount(8, 1) + 1
+            ElseIf rollValue = 10 Then
+                rollCount(9, 1) = rollCount(9, 1) + 1
+            ElseIf rollValue = 11 Then
+                rollCount(10, 1) = rollCount(10, 1) + 1
+            ElseIf rollValue = 12 Then
+                rollCount(11, 1) = rollCount(11, 1) + 1
+            Else
+                Console.WriteLine("oops")
+            End If
+
+            numberofRolls = numberofRolls + 1
+        Loop
+        ListBox1.Items.Add("Dice Rolls by Number of Times That a Roll Occurs")
+        Dim label As String
+        Dim Value As String
+
+        For printcount = 0 To 11 Step 1
+            label = label & (Str(rollCount(printcount, 0)).PadLeft(5))
+        Next
+        ListBox1.Items.Add(label)
+        ListBox1.Items.Add("")
+        For printcount = 0 To 11 Step 1
+            Value = Value & (Str(rollCount(printcount, 1)).PadLeft(4))
+        Next
+        ListBox1.Items.Add(Value)
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ListBox1.Items.Clear()
+    End Sub
+End Class
